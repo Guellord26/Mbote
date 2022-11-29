77b93ea42cfae681c057fd70d0ef0d8fe1df1bd1
Guellord26 <58660059+Guellord26@users.noreply.github.com>
2022-11-30 00:17:35 +0100

    30/11 00:17

--- pages/articles.tsx
+++ pages/articles.tsx
@@ -12,7 +12,7 @@ import {
 } from "@mui/material";
 import { articles } from "../src/mock/imgesApi";
 // import { ArticleListToolbar } from "../src/Components/Article/article-list-toolbar";
-import { ArticleCard } from "../src/Components/Article/article-card";
+// import { ArticleCard } from "../src/Components/Article/article-card";
 import { DashboardLayout } from "../src/Components/Dashboard/dashboard-layout";
 import { PaginatedItems } from "../src/Components/Pagnations/Pagination";
 import Image from "next/image";
