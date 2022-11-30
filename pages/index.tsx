bd567d4b606fabdad32d58880a56d70228327283
Guellord26 <58660059+Guellord26@users.noreply.github.com>
2022-11-30 09:57:45 +0100

    10:57

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,10 +6,16 @@ import {Card, CardContent, CardHeader, Divider, useTheme } from '@mui/material';
 import { Chart } from 'react-chartjs-2';
 // import { Chart as ChartJS, registerables } from 'chart.js';
 // ChartJS.register(...registerables);
-import { Chart as ChartJS, LineController, BarElement, LineElement, PointElement, LinearScale, CategoryScale, Title } from 'chart.js';
+import { Chart as ChartJS, ArcElement, Tooltip, Legend, LineController, BarElement, LineElement, PointElement, LinearScale, CategoryScale, Title } from 'chart.js';
 
 ChartJS.register(LineController, BarElement, LineElement, PointElement, LinearScale, CategoryScale, Title);
 // ======================
+import PhoneIcon from '@mui/icons-material/Phone';
+import TabletIcon from '@mui/icons-material/Tablet';
+// import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+import { Doughnut } from 'react-chartjs-2';
+
+ChartJS.register(ArcElement, Tooltip, Legend);
 import { Avatar } from '@mui/material';
 import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
 import VisibilityIcon from '@mui/icons-material/Visibility';
@@ -22,8 +28,70 @@ export default function  Mbote () {
 
   const router = useRouter()
   const theme = useTheme();
-
+  // ================
+  
   const data = {
+    datasets: [
+      {
+        data: [63, 15, 23],
+        backgroundColor: ['#3F51B5', '#e53935', '#FB8C00'],
+        borderWidth: 8,
+        borderColor: '#FFFFFF',
+        hoverBorderColor: '#FFFFFF'
+      }
+    ],
+    labels: ['PC', 'Tablet', 'Mobile']
+  };
+  
+  const devices = [
+    {
+      title: 'Pc',
+      value: 63,
+      icon: LaptopMacIcon,
+      color: '#3F51B5'
+    },
+    {
+      title: 'Tablet',
+      value: 15,
+      icon: TabletIcon,
+      color: '#E53935'
+    },
+    {
+      title: 'Mobile',
+      value: 23,
+      icon: PhoneIcon,
+      color: '#FB8C00'
+    }
+  ];
+  const chartRef = React.useRef(null);
+  const [chartData, setChartData] = React.useState({
+    datasets: [
+      {
+        data: [63, 15, 22],
+        backgroundColor: ['#3F51B5', '#e53935', '#FB8C00'],
+        borderWidth: 8,
+        borderColor: '#FFFFFF',
+        hoverBorderColor: '#FFFFFF'
+      }
+    ],
+    labels: ['PC', 'Tablet', 'Mobile']
+  });
+  
+  React.useEffect(() => {
+    const chart = chartRef.current;
+  
+    if (chart) {
+      setChartData({
+        datasets: [{
+            backgroundColor: createBackgroundGradient(chart.ctx),
+            // ...
+        }]
+      });
+    }
+  }, []);
+  // =============================
+
+  const dataBar = {
     datasets: [
       {
         backgroundColor: '#3F51B5',
@@ -344,7 +412,7 @@ export default function  Mbote () {
           }}
         >
          
-           <Chart type='bar' data={data} />
+           <Chart type='bar' data={dataBar} />
         </Box>
        
       </CardContent>
@@ -359,6 +427,64 @@ export default function  Mbote () {
             xs={12}
           >
             {/* <TrafficByDevice /> */}
+            <Card >
+      <CardHeader title="Traffique par appareil" />
+      <Divider />
+      <CardContent>
+        <Box
+          sx={{
+            height: 275,
+            position: 'relative'
+          }}
+        >
+          
+          <Doughnut
+              data={chartData}
+              options={{
+                responsive: true,
+                maintainAspectRatio: true,
+              }}
+               />
+        </Box>
+        <Box
+          sx={{
+            display: 'flex',
+            justifyContent: 'center',
+            pt: 2
+          }}
+        >
+          {devices.map(({
+            color,
+            icon: Icon,
+            title,
+            value
+          }) => (
+            <Box
+              key={title}
+              sx={{
+                p: 1,
+                textAlign: 'center'
+              }}
+            >
+              <Icon color="action" />
+              <Typography
+                color="textPrimary"
+                variant="body1"
+              >
+                {title}
+              </Typography>
+              <Typography
+                style={{ color }}
+                variant="h4"
+              >
+                {value}
+                %
+              </Typography>
+            </Box>
+          ))}
+        </Box>
+      </CardContent>
+    </Card>
           </Grid>
         
         </Grid>
