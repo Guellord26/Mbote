dbf1d600274c99148f13ec05ee05836f30a466af
Guellord26 <58660059+Guellord26@users.noreply.github.com>
2022-11-30 07:56:37 +0100

    8:56

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { DashboardLayout } from '../src/components/dashboard/dashboard-layout';
-// import PageView from '../src/components/statistiques/page_view'
+import PageView from '../src/components/statistiques/page_view'
 
 
 export default function  Mbote () {
@@ -8,7 +8,7 @@ export default function  Mbote () {
     return (
       <>
       <h1>Bonjour</h1>
-       {/* <PageView /> */}
+       <PageView />
       </>
     )
 }
