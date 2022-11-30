c180acaa1378f97f6ae09dba9a58060964eb0b46
Guellord26 <58660059+Guellord26@users.noreply.github.com>
2022-11-30 06:37:53 +0100

    7:37

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,4 @@
 import * as React from 'react';
-import {Box, Grid, Container, Typography} from "@mui/material"
-import { TotalCustomers } from '../src/components/statistiques/total-customers';
-import { TotalProfit } from '../src/components/statistiques/total-profit';
-import { TrafficByDevice } from '../src/components/statistiques/traffic-by-device';
-import { Budget } from '../src/components/statistiques/budget';
-import { Sales } from '../src/components/statistiques/sales';
 import { DashboardLayout } from '../src/components/dashboard/dashboard-layout';
 import PageView from '../src/components/statistiques/page_view'
 
