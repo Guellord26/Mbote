66a3bb5a30259ecf8850fdd6c95bff14cd205d9d
Guellord26 <58660059+Guellord26@users.noreply.github.com>
2022-12-19 14:55:26 +0100

    15:55

--- pages/readArticle.tsx
+++ pages/readArticle.tsx
@@ -1,20 +1,135 @@
+import React from "react";
+import { useRouter } from "next/router";
 import Head from 'next/head';
 import { DashboardLayout } from '../src/Components/Dashboard/dashboard';
-import { Lecture } from "../src/Components/Article/lecture"
+import { LoremIpsum } from 'react-lorem-ipsum';
+import { articles } from "../src/mock/imgesApi";
+import { Box, Container} from '@mui/material';
+import { loadComponents } from "next/dist/server/load-components";
+import { publicites } from "../src/mock/PubApis"
+import FacebookIcon from '@mui/icons-material/Facebook';
+import TwitterIcon from '@mui/icons-material/Twitter';
 
-function ReadArticle(){
+const url = "https://newsapi.org/v2/everything";
 
-      return( 
-        <>
-          <Head>
-            <title>
-              Article | Mbote
-            </title>
-          </Head>     
-          <Lecture/>   
-        </>
-      )
-};
+export function ReadArticle() {
+  
+  const [loading, setLoading] = React.useState(true);
+  const [myArticle, setMyArticle] = React.useState([]);
+
+  const router = useRouter();
+  const { id } = router.query;
+  var idArticle = Number(id)
+
+  console.log(typeof idArticle);
+
+  function loadSkeleton() {
+    setLoading(false);
+  }
+  React.useEffect(() => {
+    const timer = setTimeout(() => loadSkeleton, 3000);
+  }, []);
+  
+ const lecture = (id) => {
+    router.push({
+      pathname: '/readArticle',
+      query: { id: id },
+  })
+   }
+
+  return (
+    <>
+    <Head>
+      <title>
+        Lecture
+      </title>
+    </Head>
+  
+    <Container>
+      <div className="2xl:mx-auto xl:px-20 lg:px-10 px-6 py-2  rounded-lg bg-white p-8">
+        <div className="lg:flex justify-center">
+          {/* ======================================================= */}
+          <div className="2xl:w-7/12 lg:w-8/12 md:w-10/12">
+            {articles.map((item, index) => (
+              <div key={index}>
+                {idArticle == item.id && (
+                  <>
+                    <img
+                      src={item.media}
+                      alt="fingerprint recognition"
+                      className="lg:w-full w-auto"
+                    />
+                    <div className="mt-4">
+                      <div className="flex items-center justify-between">
+                        <div className="flex items-center">
+                          <p className="text-base leading-4 text-gray-500">
+                            {item.createdAt}
+                          </p>
+                          <p className="text-base leading-none text-gray-500 ml-12">
+                            {item.nbVues} Lecture
+                          </p>
+                        </div>
+                        <div className="flex items-center">
+                          {/* <svg
+                            width={64}
+                            height={2}
+                            viewBox="0 0 64 2"
+                            fill="none"
+                            xmlns="http://www.w3.org/2000/svg"
+                          >
+                            <path d="M64 1H0" stroke="#6B7280" />
+                          </svg> */}
+                          <p className="text-base leading-none text-gray-500 ml-2">
+                          <a href="">
+                             <FacebookIcon /> Facebook
+                          </a> {'  '}
+                          <a href="">
+                             <TwitterIcon /> Twitter
+                          </a>
+                          </p>
+                          
+                        </div>
+                      </div>
+                      <h1 className="lg:text-3xl text-2xl font-semibold mt-4 text-gray-800">
+                        {item.title}
+                      </h1>
+                      <h3 className=" font-semibold mt-4 text-gray-700">
+                        {item.description}
+                      </h3>
+                      <p className="text-base leading-6 text-gray-600 mt-2"><LoremIpsum p={item.contents} /></p>
+                      {/* <div className="text-wrapper">
+                           <LoremIpsum p={item.contents} />
+                      </div>  */}
+                    </div>
+                  </>
+                )}
+              </div>
+            ))}
+          </div>
+          {/* ======================================================= */}
+          <div className="lg:ml-6 2xl:w-3/12 lg:w-4/12 w-full flex lg:flex-col sm:flex-row flex-col items-center">
+            {publicites.map((item, index) => (
+              <div key={index} style={{marginBottom: "10px"}} >
+                {idArticle != item.id && (
+                  <div className="lg:w-auto sm:w-1/2">
+                    <img
+                      src={item.media}
+                      alt="flying letters"
+                      className="w-full"
+                    />
+                  </div>
+                )}
+              </div>
+            ))}
+            
+          </div>
+        </div>
+      </div>
+    </Container>
+    </>
+  );
+}
+
 
 ReadArticle.getLayout = (page) => (
   <DashboardLayout >
